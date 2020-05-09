fb8ff0fd4189513a843b69eaf255ab45d027dbef
John fortnite kennedy <kryplap192@gmail.com>
2020-05-09 18:57:25 -0400

    Update linux_build.sh

--- buildscripts/linux_build.sh
+++ buildscripts/linux_build.sh
@@ -1,7 +1,7 @@
 odir="$PWD"
 cd ..
 if [ "$1" == save ]; then
-    (g++ -O3 -std=c++2a -o jfkmarioworld JFKMarioWorld.cpp -lSDL2 -lSDL2_mixer -lSDL2_image -llua5.3 -I/usr/include/SDL2 -I/usr/include/lua5.3 -I/usr/include/SFML) > outfile 2>&1
+    (g++ -O3 -std=c++2a -o jfkmarioworld JFKMarioWorld.cpp -lSDL2 -lSDL2_mixer -lSDL2_image -llua5.3 -I/usr/include/SDL2 -I/usr/include/lua5.3 -I/usr/include/SFML) > buildscripts/outfile 2>&1
 else
     g++ -O3 -std=c++2a -o jfkmarioworld JFKMarioWorld.cpp -lSDL2 -lSDL2_mixer -lSDL2_image -llua5.3 -I/usr/include/SDL2 -I/usr/include/lua5.3 -I/usr/include/SFML
 fi
