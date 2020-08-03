9cb6e97c060b3920d677edd0c2a7d9fbd3ea88b0
john fortnite kennedy R <>
2020-08-03 01:55:09 -0300

    Update map16block.h

--- map16block.h
+++ map16block.h
@@ -60,7 +60,42 @@ public:
 	uint_fast16_t set_to = 0x025;
 	uint_fast16_t x = 0;
 	uint_fast16_t y = 0;
-	uint_fast8_t time = 0xFF;
+	uint_fast16_t time = 0xFF;
+	
+
+	bool has_spr = false;
+	uint_fast8_t spr_tile = 0;
+	uint_fast8_t pal_props = 0;
+	double spr_x = 0;
+	double spr_y = 0;
+	double spr_sx = 0;
+	double spr_sy = 0;
+
+	void draw()
+	{
+		spr_sy -= Calculate_Speed(256);
+		spr_x += spr_sx;
+		spr_y += spr_sy;
+
+		uint_fast16_t oam_index = 0;
+		uint_fast16_t s_x = uint_fast16_t(spr_x);
+		uint_fast16_t s_y = uint_fast16_t(spr_y);
+		while (oam_index < 0x400)
+		{
+			if (RAM[0x200 + oam_index] == 0 && RAM[0x206 + oam_index] == 0) { //Empty OAM slot found
+				break;
+			}
+			oam_index += 8;
+		}
+		RAM[0x200 + oam_index] = spr_tile;
+		RAM[0x201 + oam_index] = 0x11;
+		RAM[0x202 + oam_index] = s_x;
+		RAM[0x203 + oam_index] = s_x >> 8;
+		RAM[0x204 + oam_index] = s_y;
+		RAM[0x205 + oam_index] = s_y >> 8;
+		RAM[0x206 + oam_index] = pal_props;
+		RAM[0x207 + oam_index] = 0;
+	}
 };
 vector<block_timer> blocks_processing;
 
@@ -164,22 +199,26 @@ public:
 
 
 			uint_fast16_t t = RAM[ram_level_low + index] + (RAM[ram_level_high + index] << 8);
-			if (t == 0x0124 && side == bottom)
-			{
-				replace_map_tile(0x0132, x, y);
-			}
 			if (t == 0x11E && side == bottom)
 			{
-				blocks_processing.push_back(block_timer{ 0x11E, x, y, 0xFF });
-				replace_map_tile(0x48, x, y);
+				blocks_processing.push_back(block_timer{ 0x48, x, y, 0x4, true, 0x40, 0x8, double(x * 16), double(y * 16) - 17.0, 0.0, 4.0 });
+				blocks_processing.push_back(block_timer{ 0x11E, x, y, 0x100+4 });
+				replace_map_tile(0xFF, x, y);
+			}
+
+			if (t == 0x0124 && side == bottom)
+			{
+				replace_map_tile(0xFF, x, y);
 			}
 			if (t == 0x011F && side == bottom)
 			{
-				replace_map_tile(0x0132, x, y);
-				x *= 16;
-				y *= 16;
-				uint_fast8_t spr = spawnSpriteJFKMarioWorld(0x74, 5, x, y + 8, 1, true);
+				replace_map_tile(0xFF, x, y);
+				uint_fast8_t spr = spawnSpriteJFKMarioWorld(0x74, 5, x * 16, 8 + y * 16, 1, true);
 				RAM[0x2480 + spr] = 0x20;
+			}
+			if (t == 0x11F || t == 0x124) //With bounce sprites
+			{
+				blocks_processing.push_back(block_timer{ 0x132, x, y, 0x4, true, 0x2A, 0x8, double(x * 16), double(y * 16)-17.0, 0.0, 4.0});
 			}
 			if (t == 0x0112 && side == bottom)
 			{
@@ -231,6 +270,10 @@ public:
 		for (int i = 0; i < blocks_processing.size(); i++)
 		{
 			block_timer& b = blocks_processing[i];
+			if (b.has_spr)
+			{
+				b.draw();
+			}
 			b.time--;
 			if (b.time == 0)
 			{
